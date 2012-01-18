5572cd7b11c2c80764b8252502defea5953459df
Ben Waters <ben25890@gmail.com>
2012-01-18 14:46:04 -0500

    Added icons for the menu

---
+++ src/thesis/views/Timeline.java
@@ -0,0 +1,22 @@
+package thesis.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class Timeline extends ViewPart{
+	public static final String ID = "testview.views.SampleView";
+	
+	
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
