ba0a072baf9c94c8370da85cc14f9a0f991b4cae
Ben Waters <ben25890@gmail.com>
2012-01-30 12:42:59 -0500

    The timeline view is a simple label which lists the total time of all tests and updates when the focus is put on it

--- src/thesis/views/Timeline.java
+++ src/thesis/views/Timeline.java
@@ -1,19 +1,52 @@
 package thesis.views;
 
+import java.util.ArrayList;
+
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
+
+import thesis.Activator;
+import thesis.data.TestResult;
 
 public class Timeline extends ViewPart{
 	public static final String ID = "testview.views.SampleView";
-	private TableViewer viewer;
+	private ArrayList<TestResult> testData;
+	private Label label;
+	
+	public Timeline(){super();}
 	
 	@Override
 	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
-		
+		label = new Label(parent,0);
+		label.setText("Foobar");
 	}
 
-	public void setFocus() {viewer.getControl().setFocus();}
+	private void drawTimeLine(){
+		testData=Activator.getDefault().testList;
+		double total_time=0.0;
+		for(int x=0;x<testData.size();x++)
+			total_time+=testData.get(x).getTime();
+		label.setText(total_time+"");
+
+	}
+	public void setFocus() {
+		drawTimeLine();
+		label.setFocus();
+	}
 
 }
