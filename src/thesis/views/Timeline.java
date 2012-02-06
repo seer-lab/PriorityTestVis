9ea0834efdaefd75ccf588bf194ac595ce318d9f
Ben Waters <ben25890@gmail.com>
2012-02-06 14:50:57 -0500

    Merged updated timeline and removed unused dependencies

--- src/thesis/views/Timeline.java
+++ src/thesis/views/Timeline.java
@@ -2,23 +2,12 @@ package thesis.views;
 
 import java.util.ArrayList;
 
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.PaintEvent;
-import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.RGB;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
 import thesis.Activator;
